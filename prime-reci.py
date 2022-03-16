586072abb38c97534cae241436a17332caebab67
svenazari <94923703+svenazari@users.noreply.github.com>
2022-03-16 09:11:17 +0100

    Add files via upload

--- prime-reci.py
+++ prime-reci.py
@@ -1,6 +1,7 @@
 #Verison 1
 #Requirements: Python3
 #Version 1 only calculates how many digits is in period of decimal expansion of prime numbers. User needs to take care inputed nuber is prime.
+#Made by Sven Azari
 
 from itertools import count
 
