bc215a80453822e2ac1cdd2887e36b838257a969
Sven Azari <94923703+svenazari@users.noreply.github.com>
2022-03-16 17:49:25 +0100

    Add files via upload

--- prime-reci.py
+++ prime-reci.py
@@ -1,25 +1,82 @@
-#Verison 1
+#Verison 2
 #Requirements: Python3
-#Version 1 only calculates how many digits is in period of decimal expansion of prime numbers. User needs to take care inputed nuber is prime.
+#Version 2 checks if number is prime and then calculates how many digits is in period of decimal expansion of prime numbers.
 #Made by Sven Azari
 
+#imports
+import math
+from os import system, name
 from itertools import count
 
-x = int(input("prime = ")) #input
+#clear
+def clear ():
+  if name == 'nt':
+      _ = system('cls')
+  else:
+      _ = system('clear')
 
-li = [] #list for comparisong
-d = 1
-c = 0
+#main part
+def primetest ():
+    print ("####################")
+    print ("#                  #")
+    print ("#    PRIME RECI    #")
+    print ("#                  #")
+    print ("####################")
+    print (" ")
+    x = float(input ("X = "))
+    print (" ")
+    x1 = math.ceil(x / 2) + 1
+    if x == 1:
+      print ("NOR PRIME NOR COMPOSIT")
+      fuex ()
+    elif x == 2:
+      print ("PRIME")
+      fuex ()
+    elif x < 1:
+      print ("ENTER ONLY NATURAL NUMBERS")
+      primetest ()
+    for PT in range (2, x1, 1):
+        a = x % PT
+        if a == 0:
+            break
+    if a != 0:
+        li = [] #list for comparisong
+        d = 1
+        c = 0 #number of itirations
 
-while True: #calculation loop
-    o = d % x
-    try:
-        b = li.index(o)
-    except ValueError:
-        li.append(o)
-        d = o * 10
-        c +=1
+        while True: #calculation loop
+            o = d % x
+            try:
+                b = li.index(o)
+            except ValueError:
+                li.append(o)
+                d = o * 10
+                c +=1
+            else:
+                break
+        
+        print ("PERIOD OD DECIMAL EXPANSION")
+        print (c) #final print of length of period of decimal expansion
+        print (" ")
+        fuex ()
+
     else:
-        break
+        print ("INPUTED NUMBER IS NOT PRIME")
+        print (" ")
+        fuex ()
 
-print (c) #final print of length of period of decimal expansion
+#exit function
+def fuex ():
+  ex = float(input ("[1 (new), 2 (exit)]: "))
+  if ex == 1:
+    clear ()
+    primetest ()
+  elif ex == 2:
+    clear ()
+    exit ()
+  else:
+    print ("Wrong input!")
+    fuex ()
+
+clear ()
+primetest ()
